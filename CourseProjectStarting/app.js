a10b6fea5bf8255f09b0ae895af1013c641cfc79
Sumithpd <https://github.com/sumithpdd/flutter_complete_guide.git>
2020-11-11 14:34:01 +0100

    return types and void!

--- CourseProjectStarting/app.js
+++ CourseProjectStarting/app.js
@@ -1,17 +1,8 @@
-function combine(input1, input2, resultConversion) {
-    var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number'
-        || resultConversion === 'as-number') {
-        result = +input1 + +input2;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
+function addNumber(n1, n2) {
+    var result = n1 + n2;
     return result;
 }
-var combinedAges = combine(30, 26, 'as-number');
-console.log(combinedAges);
-var combinedStringAges = combine('30', '26', 'as-number');
-console.log(combinedStringAges);
-var combinedNames = combine('Sumith ', 'Parambat', 'as-text');
-console.log(combinedNames);
+function printAsResult(num) {
+    console.log('Result: ' + num);
+}
+printAsResult(addNumber(5, 12));
