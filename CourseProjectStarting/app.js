f15bd71233dd60a3a84396aa88318da915d3aa3c
Sumithpd <https://github.com/sumithpdd/flutter_complete_guide.git>
2020-11-11 14:43:38 +0100

    function types and callbacks

--- CourseProjectStarting/app.js
+++ CourseProjectStarting/app.js
@@ -5,4 +5,14 @@ function addNumber(n1, n2) {
 function printAsResult(num) {
     console.log('Result: ' + num);
 }
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
 printAsResult(addNumber(5, 12));
+var combineValues;
+combineValues = addNumber;
+console.log(combineValues(8, 8));
+addAndHandle(10, 20, function (result) {
+    console.log(result);
+});
