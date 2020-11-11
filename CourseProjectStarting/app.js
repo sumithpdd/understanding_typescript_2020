8ca95c2d2b588d0e359cd700a5e127fd3cc4c9ea
Sumithpd <https://github.com/sumithpdd/flutter_complete_guide.git>
2020-11-11 06:07:58 +0100

    Core types , number , boolean and string

--- CourseProjectStarting/app.js
+++ CourseProjectStarting/app.js
@@ -1,1 +1,16 @@
-console.log('The code goes here...');
+function add(n1, n2, printResult, resultPhrase) {
+    // in javascript this is how we can handle incorrect type sent to the parameter
+    if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+        throw new Error("Incorrect input!");
+    }
+    var result = n1 + n2;
+    if (printResult) {
+        console.log(resultPhrase + result.toString());
+    }
+    return result;
+}
+var number1 = 5;
+var number2 = 2.8;
+var printResult = true;
+var resultPhrase = 'Result is: ';
+add(number1, number2, printResult, resultPhrase);
--- CourseProjectStarting/readme.md
+++ CourseProjectStarting/readme.md
@@ -8,4 +8,11 @@ Run npm init so that we can add some required node packages
 
 and then add "start": "lite-server" to package.json
 to start and open the browser run =>
-npm start -o
+` npm start -o `
+
+## Important: Type Casing
+
+In TypeScript, you work with types like string or number all the times.
+
+> Important: It is string and number (etc.), NOT String, Number etc.
+> The core primitive types in TypeScript are all lowercase!
