44a9eb9948e18561512191ce09b11c4372039310
Sumithpd <https://github.com/sumithpdd/flutter_complete_guide.git>
2020-11-11 15:08:42 +0100

    unknown and never types

--- CourseProjectStarting/app.js
+++ CourseProjectStarting/app.js
@@ -1,18 +1,14 @@
-function addNumber(n1, n2) {
-    var result = n1 + n2;
-    return result;
+var userInput;
+var userName;
+userInput = 5;
+userInput = 'Max';
+if (typeof userInput === 'string') {
+    userName = userName;
 }
-function printAsResult(num) {
-    console.log('Result: ' + num);
+
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+
+    //while(true){}
 }
-function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-}
-printAsResult(addNumber(5, 12));
-var combineValues;
-combineValues = addNumber;
-console.log(combineValues(8, 8));
-addAndHandle(10, 20, function (result) {
-    console.log(result);
-});
+generateError('An error occurred!', 500);
--- CourseProjectStarting/app.ts
+++ CourseProjectStarting/app.ts
@@ -1,23 +1,15 @@
-function addNumber(n1 : number, n2 :number) { 
-   const result = n1+n2;
+let userInput: unknown;
+let userName:string;
 
-    return result;
+userInput=5;
+userInput ='Max';
+
+if(typeof userInput==='string'){
+    userName=userName;
 }
-function printAsResult(num:number):void{
-    console.log('Result: ' + num);
+
+function generateError(message:string,code:number):never{
+    throw {message: message,errorCode:code};
+    
 }
-
-function addAndHandle(n1:number,n2 :number,cb:(num:number)=>void){
-    const result = n1+n2;
-    cb(result);
-}
-printAsResult(addNumber(5,12));
-
-let combineValues: (a:number, b:number)=>number;
-
-combineValues=addNumber;
-console.log(combineValues(8,8));
-
-addAndHandle(10,20 ,(result)=>{
-    console.log(result);
-})
+generateError('An error occurred!' ,500);
