9a20af1f942ec6f1297990a7bfbe55b8a60437fd
Sumithpd <https://github.com/sumithpdd/flutter_complete_guide.git>
2020-11-11 14:17:45 +0100

    example of literal types

--- CourseProjectStarting/app.ts
+++ CourseProjectStarting/app.ts
@@ -1,8 +1,13 @@
-function combine(input1 : number | string, input2 :number | string) 
+function combine(input1 : number | string, 
+    input2 :number | string,
+    resultConversion:'as-number'|'as-text'
+    ) 
 { 
 let result;
-if(typeof input1 ==='number' && typeof input2 ==='number')    {
-    result =input1+input2;
+if(typeof input1 ==='number' && typeof input2 ==='number'
+|| resultConversion==='as-number'
+)    {
+    result = +input1 + +input2;
 
 }else{
     result =input1.toString()+input2.toString();
@@ -10,8 +15,11 @@ if(typeof input1 ==='number' && typeof input2 ==='number')    {
   
     return result;
 }
-const combinedAges = combine(30,26);
+const combinedAges = combine(30,26,'as-number');
 console.log(combinedAges);
 
-const combinedNames= combine('Sumith ','Parambat');
+const combinedStringAges = combine('30','26','as-number');
+console.log(combinedStringAges);
+
+const combinedNames= combine('Sumith ','Parambat','as-text');
 console.log(combinedNames);
